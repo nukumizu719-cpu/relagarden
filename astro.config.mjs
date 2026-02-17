deff0a7d90675986d0ba9a2837f129606f323c68
GEN KITAMURA <nukumizu719@gmail.com>
2026-02-18 07:26:05 +0900

    feat: サイトマップの生成を確認

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,16 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
+  // ↓ ここを追加！サイトマップ生成に必須です
+  site: 'https://relagarden.jp',
+
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+
+  integrations: [sitemap()]
 });
